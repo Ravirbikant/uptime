85f123c2b8e84aef83ad83c7724899c33b8d8c9f
Ravi Kant Jha <ravirbikant@gmail.com>
2026-02-15 11:15:29 +0530

    Create top bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css";
+import Top from "./Top";
+
+function Navbar() {
+  return (
+    <header className="navbar-header">
+      <Top />
+    </header>
+  );
+}
+
+export default Navbar;
