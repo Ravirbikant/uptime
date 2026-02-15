28dbaddaf1dabfd2907ff2869a9b65b7e8387060
Ravi Kant Jha <ravirbikant@gmail.com>
2026-02-15 11:24:09 +0530

    Add tabs component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import "./navbar.css";
 import Top from "./Top";
+import ProfileTabs from "./ProfileTabs";
 
 function Navbar() {
   return (
     <header className="navbar-header">
       <Top />
+      <ProfileTabs />
     </header>
   );
 }
