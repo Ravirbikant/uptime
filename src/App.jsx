d885a4e582afaa265e043bb548a8d200aac45818
Ravi Kant Jha <ravirbikant@gmail.com>
2026-03-02 13:06:41 +0530

    Basic app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-import { ProfileProvider } from "./context/ProfileContext";
-import { ToastProvider } from "./context/ToastContext";
-import Navbar from "./components/Navbar";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
-
 function App() {
-  const [activeTabId, setActiveTabId] = useState("overview");
-
-  return (
-    <ProfileProvider>
-      <ToastProvider>
-        <Navbar activeTabId={activeTabId} onTabChange={setActiveTabId} />
-        <Body activeTabId={activeTabId} />
-        <Footer />
-      </ToastProvider>
-    </ProfileProvider>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import "./loader.css";
-
-export default function Loader() {
-  return (
-    <div className="loader-page">
-      <div className="loader-spinner" />
-    </div>
-  );
-}
